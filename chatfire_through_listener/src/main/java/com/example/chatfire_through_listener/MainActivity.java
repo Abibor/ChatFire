770455ea89ebb7d9a6bffe406b7368805fc2fbea
Boris Abiduev <abidyev@gmail.com>
2022-02-24 07:44:43 +0800

    Home

---
+++ chatfire_through_listener/src/main/java/com/example/chatfire_through_listener/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.chatfire_through_listener;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
